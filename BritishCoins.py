c5415249cca76e5cf5ae1442dcf53a3d7cad4248
hamncheese39 <haharrell@packer.edu>
2020-11-11 18:04:36 -0500

    Update BritishCoins.py

--- BritishCoins.py
+++ BritishCoins.py
@@ -1,14 +1,23 @@
 total = 0
-for one in range(200):
-  for two in range(100):
-    for five in range(40):
-      for ten in range(20):
-        for twenty in range(10):
-          for fifty in range(4):
-            for pound in range(2):
-              for twoPound in range(1):
-                sumn = one + 2*two + 5*five + 10*ten + 20*twenty + 50*fifty + 100*pound + 200*twoPound
-                if sumn == 200:
-                  total += 1
-                  print(one, two, five, ten, twenty, fifty, pound, twoPound)
+for ones in range(201):
+  sumn = 1*ones
+  for twos in range(int((200-sumn)/2)+1):
+    sumn = 1*ones + 2*twos
+    for fives in range(int((200-sumn)/5)+1):
+      sumn = 1*ones + 2*twos + 5*fives
+      for tens in range(int((200-sumn)/10)+1):
+        sumn = 1*ones + 2*twos + 5*fives + 10*tens
+        for twenties in range(int((200-sumn)/20)+1):
+          sumn = 1*ones + 2*twos + 5*fives + 10*tens + 20*twenties
+          for fifties in range(int((200-sumn)/50)+1):
+            sumn = 1*ones + 2*twos + 5*fives + 10*tens + 20*twenties + 50*fifties
+            for pounds in range(int((200-sumn)/100)+1):
+              sumn = 1*ones + 2*twos + 5*fives + 10*tens + 20*twenties + 50*fifties + 100*pounds
+              #print(sumn, ones, twos, fives, tens, twenties, fifties, pounds)
+              if sumn == 200:
+                total += 1
+                if total % 1000 == 0:
+                  print("good!", ones, twos, fives, tens, twenties, fifties, pounds)
+
+total += 1 #for the one with just two pounds              
 print("total amount =", total)
